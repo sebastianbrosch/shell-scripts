a4b537970603e26a5513f228e8f23585cc6a1e5c
Sebastian Brosch <code@sebastianbrosch.de>
2023-07-18 21:23:40 +0200

    added new function is_swarm_manager

--- docker/lib-docker.sh
+++ docker/lib-docker.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-# ------------------------------------------------------------------------
-# This script is a collection of useful functions for Docker.
-# ------------------------------------------------------------------------
-
-# function to check whether a container with a specified ID or name exists.
+#
+# A collection of useful functions for Docker.
+#
+# function to check whether a container with the specified ID or name exists.
 container_exists() {
-  local container
+  local container id name
   container=$1
-  id=$(docker ps --format="{{.ID}}" | grep -cxF "$container")
-  name=$(docker ps --format="{{.Names}}" | grep -cxF "$container")
+  id=$(docker ps --all --format="{{.ID}}" | grep -cxF "$container")
+  name=$(docker ps --all --format="{{.Names}}" | grep -cxF "$container")
   [[ "$id" == "1" || "$name" == "1" ]]
 }
 
 # function to get the state of a specific container.
 get_container_state() {
-  docker ps --format="{{.ID}} {{.State}}" | awk -v container="$1" '{ if($1 == container) print $2 }'
+  docker ps --all --format="{{.ID}} {{.State}}" | awk -v container="$1" '{ if($1 == container) print $2 }'
+}
+
+# function to get the state whether this server is a swarm manager.
+is_swarm_manager() {
+  local state
+  state=$(docker info --format '{{.Swarm.ControlAvailable}}')
+  [[ "$state" == "true" ]]
 }
