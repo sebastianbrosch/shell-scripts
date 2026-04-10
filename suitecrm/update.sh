65eab253aead331e6d131bd657a98e521adda890
Sebastian Brosch <code@sebastianbrosch.de>
2026-04-11 01:00:46 +0200

    improved script and updated to current version

--- suitecrm/update.sh
+++ suitecrm/update.sh
@@ -9,7 +9,7 @@
 #  -c <container> Run the script on the specified container.
 #  -C             Run the script without a specified container.
 #  -v <version>   The version of SuiteCRM which should be installed.
-#  -V             Updated SuiteCRM to the latest version (8.4.1).
+#  -V             Updates SuiteCRM to the latest version.
 #  -h             Shows the help information of this script.
 #
 # Exit Codes (1X = Script; 2X = Docker; 3X = SuiteCRM):
@@ -18,16 +18,17 @@
 #  10             There are invalid parameters.
 #  11             The version was not specified.
 #  12             Changing to SuiteCRM folder failed.
-#  13             No paramerer was specified.
+#  13             No parameter was specified.
 #  20             The container is not known.
 #  21             The container is not running.
 #  22             The script could not be injected into the container.
 #  30             The current version of SuiteCRM can not be updated.
-#  31             The specified version for update is not known.
+#  31             No valid version specified.
+#  32             The package for the specified version was not found.
 # ------------------------------------------------------------------------
 
 # set the latest version.
-SUITECRM_LATEST=8.7.1
+SUITECRM_LATEST=8.9.3
 
 # function to output the help information to the terminal.
 show_help() {
@@ -39,7 +40,7 @@ show_help() {
   printf "%-20s%-s\n" "  -c <container>" "Run the script on the specified container."
   printf "%-20s%-s\n" "  -C" "Run the script without a specified container."
   printf "%-20s%-s\n" "  -v <version>" "The version of SuiteCRM which should be installed."
-  printf "%-20s%-s\n" "  -V" "Updates SuiteCRM to the latest version (${SUITECRM_LATEST})."
+  printf "%-20s%-s\n" "  -V" "Updates SuiteCRM to the latest version ($SUITECRM_LATEST)."
   printf "%-20s%-s\n" "  -h" "Shows the help information in the terminal."
   printf "\n\n"
 }
@@ -56,6 +57,14 @@ container_exists() {
 # function to get the state of a specific container.
 get_container_state() {
   docker ps --format="{{.ID}} {{.State}}" | awk -v container="$1" '{ if($1 == container) print $2 }'
+}
+
+# function to get the state whether the specified package exists.
+package_exists() {
+  local url
+  url=$1
+  code=$(curl -s -o /dev/null -w "%{http_code}" "${url}")
+  [[ "$code" == "302" || "$code" == "200" ]]
 }
 
 # function to inject the script to the specified container.
@@ -116,7 +125,7 @@ then
     printf "Script could not be injected into the container.\n"
     exit 22
   else
-    docker exec -it "$container" bash -c "./tmp/update.sh -v $version"
+    docker exec -it "$container" bash -c "/tmp/update.sh -v \"$version\""
     exit $?
   fi
 fi
@@ -132,33 +141,28 @@ cd bitnami/suitecrm/ || exit 12
 cversion=$(cat VERSION)
 
 # check if the current version of SuiteCRM can be updated.
-if [[ ! $cversion =~ ^8\.[234567]\.[0-9]$ ]]
+if [[ ! "$cversion" =~ ^8\.[2-9]\.[0-9]$ ]]
 then
   printf "This version can not be updated.\n"
   exit 30
 fi
 
-# check whether the version is known.
-case $version in
-  8.2.0) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.2.0/SuiteCRM-8.2.0.zip;;
-  8.2.1) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.2.1/SuiteCRM-8.2.1.zip;;
-  8.2.2) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.2.2/SuiteCRM-8.2.2.zip;;
-  8.2.3) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.2.3/SuiteCRM-8.2.3.zip;;
-  8.2.4) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.2.4/SuiteCRM-8.2.4.zip;;
-  8.3.0) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.3.0/SuiteCRM-8.3.0.zip;;
-  8.3.1) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.3.1/SuiteCRM-8.3.1.zip;;
-  8.4.0) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.4.0/SuiteCRM-8.4.0.zip;;
-  8.4.1) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.4.1/SuiteCRM-8.4.1.zip;;
-  8.4.2) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.4.2/SuiteCRM-8.4.2.zip;;
-  8.5.0) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.5.0/SuiteCRM-8.5.0.zip;;
-  8.5.1) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.5.1/SuiteCRM-8.5.1.zip;;
-  8.6.0) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.6.0/SuiteCRM-8.6.0.zip;;
-  8.6.1) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.6.1/SuiteCRM-8.6.1.zip;;
-  8.6.2) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.6.2/SuiteCRM-8.6.2.zip;;
-  8.7.0) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.7.0/SuiteCRM-8.7.0.zip;;
-  8.7.1) url=https://github.com/salesagility/SuiteCRM-Core/releases/download/v8.7.1/SuiteCRM-8.7.1.zip;;
-  *) printf "No valid version specified.\n"; exit 31;;
-esac
+# check if the specified version for the package is a valid version.
+if [[ ! "$version" =~ ^8\.[2-9]\.[0-9]$ ]]
+then
+  printf "No valid version specified.\n"
+  exit 31
+fi
+
+# create the url for the package of the specified version.
+url="https://github.com/SuiteCRM/SuiteCRM-Core/releases/download/v$version/SuiteCRM-$version.zip"
+
+# check whether the package of the version exists.
+if ! package_exists "$url"
+then
+  printf "The package for the specified version was not found.\n"
+  exit 32
+fi
 
 # output some information about the parameters.
 printf "\n\n"
@@ -171,7 +175,7 @@ mkdir -p tmp/package/upgrade
 
 # download the specified version.
 printf "Download SuiteCRM %s ...\n" "$version"
-curl -L -s -S $url -o "tmp/package/upgrade/SuiteCRM-$version.zip"
+curl -L -s -S "$url" -o "tmp/package/upgrade/SuiteCRM-$version.zip"
 printf "Dowloaded SuiteCRM %s.\n" "$version"
 
 # to get the information about permissions you can use ls -l in public folder.
